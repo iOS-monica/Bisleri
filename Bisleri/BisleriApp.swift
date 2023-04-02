ed5b63fc99d5d83008f148136d2f4e570053eea9
Monica N <monica@softsuave.com>
2023-04-02 12:26:52 +0530

    initial commit

---
+++ Bisleri/BisleriApp.swift
@@ -0,0 +1,17 @@
+//
+//  BisleriApp.swift
+//  Bisleri
+//
+//  Created by SOFTSUAVE on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BisleriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bisleri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bisleri
+//
+//  Created by SOFTSUAVE on 02/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
